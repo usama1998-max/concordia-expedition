afd906592263910b3f907ef855d19076197fcf1b
usama1998-pro <154095855+usama1998-pro@users.noreply.github.com>
2025-05-16 21:21:22 +0500

    form connected to microservice

--- app/lib/microservice.ts
+++ app/lib/microservice.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+  baseURL: 'https://concordia-expedition-microservice.vercel.app/',
   headers: {
     'Content-Type': 'application/json',
   },
